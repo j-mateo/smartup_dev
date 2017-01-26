38fa3b92622bdeb4ffe399c7db925b965418cb20
fishlegz <velvetwall@protonmail.com>
2017-01-26 00:36:49 +0000

    Inital commit, add jade support

---
+++ app/scripts/main.js
@@ -0,0 +1,45 @@
+console.log('\'Allo \'Allo!');
+
+//=======================================================
+// Video Section
+//=======================================================
+$(document).ready(function() {
+
+    // Animation druing scroll
+    window.sr = ScrollReveal();
+    sr.reveal('.reveal', {
+        duration: 700
+    });
+
+    var vid1 = document.getElementById("myVideo1");
+    if (vid1) {
+        function playPause1() {
+            if (vid1.paused) {
+                vid1.play();
+            }
+            else {
+                vid1.pause();
+            }
+        }
+        if ($("#videoWrap1").length > 0) {
+            $('#playVideos').on('click', function(e) {
+                e.preventDefault();
+                playPause1();
+                if ($(this).hasClass('active')) {
+                    $(this).removeClass('active');
+                    vid1.pause();
+                }
+                else {
+                    $(this).addClass('active');
+                    vid1.play();
+                }
+            });
+        }
+        vid1.play();
+
+        // $('.play-btn').on('click', function(e) {
+        //     e.preventDefault();
+        //     vid1.play();
+        // });
+    }
+});
